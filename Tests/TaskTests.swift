f9edd51708cab9cfd9cfb2f6015fc6ad969feeb9
Zachary Waldowski <zach@waldowski.me>
2016-04-15 15:14:59 -0500

    Overloads of flatMap for the success value of TaskType

---
+++ Tests/TaskTests.swift
@@ -0,0 +1,31 @@
+//
+//  TaskTests.swift
+//  DeferredTests
+//
+//  Created by John Gallagher on 7/1/15.
+//  Copyright © 2015-2016 Big Nerd Ranch. Licensed under MIT.
+//
+
+import XCTest
+#if SWIFT_PACKAGE
+import Result
+import Deferred
+@testable import Task
+#else
+@testable import Deferred
+#endif
+
+class CancellableTaskTests: XCTestCase {
+
+    func testThatFlatMapForwardsCancellationToSubsequentTask() {
+        let firstTask = Task<Int>(value: .Success(1))
+        let expectation = expectationWithDescription("flatMapped task is cancelled")
+        let mappedTask = firstTask.flatMap { _ -> Task<Int> in
+            let d = Deferred<TaskResult<Int>>()
+            return Task(d, cancellation: expectation.fulfill)
+        }
+        mappedTask.cancel()
+        waitForExpectationsWithTimeout(TestTimeout, handler: nil)
+    }
+
+}
